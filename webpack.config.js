52d161eda5ba8ff096faa3b712c3ca227ff9689f
shinjiyun <ranji12@naver.com>
2024-07-09 10:10:27 +0900

    ts-loader설치

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: `${name}.bundle.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/, //ts인지 확인할거야
+        use: "ts-loader", //ts-loader 쓸거야
+        exclude: /node_modules/ //제외할거야
+      }
+    ],
+  },
+  resolve: { //이행하다
+    extensions: [".ts", ".js"]
   }
 }
