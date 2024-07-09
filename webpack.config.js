2a0da9fba2b47fa4c25568f6dfc1366702b97854
shinjiyun <ranji12@naver.com>
2024-07-09 10:16:33 +0900

    babel설치

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
-let name = "app";
+let name = "test";
 module.exports = {
-  entry: `./src/${name}.js`,
+  entry: `./src/${name}.ts`,
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: `${name}.bundle.js`
   },
   module: {
     rules: [
+      {
+        test:/\.js$/,
+        use:"babel-loader",
+        exclude:/node_modules/
+      },
       {
         test: /\.ts/, //ts인지 확인할거야
         use: "ts-loader", //ts-loader 쓸거야
