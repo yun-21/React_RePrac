f04b64ef805dba14675f4196c791c9ad734e26e9
shinjiyun <ranji12@naver.com>
2024-07-09 14:41:54 +0900

    부품 조립

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import ul from '../static/ul'
 import minam from '../static/basic-data'
+import Ul from './Ul' //컴포넌트 부품
 import Minam from './Minam' //컴포넌트 부품
+import Bin from './Bin'
+import style from '../static/style'
 
 
 //파스칼케이스
 const App:React.FC =() =>{
   return (
     <div>
+      <Ul textNode={ul}/>
       <Minam textNode={minam}/>
+      <Bin styles={style} h1TextNode='헬로우'/>
       <h1>안녕</h1>
     </div>
   );
