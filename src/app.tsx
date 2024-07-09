bf6c6eb78358a68d7b4b1d6615629e799b0f7796
shinjiyun <ranji12@naver.com>
2024-07-09 11:42:37 +0900

    app.tsx 작성

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Minam = () => {
+  return (
+    <h1>욱공재</h1>
+  );
+}
+//파스칼케이스
+const App:React.FC =() =>{
+  return (
+    <div>
+      <Minam/>
+      <h1>안녕</h1>
+    </div>
+  );
+}
+
+export default App;
